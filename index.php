43347c4649a747259c4a320bc91aa5cfc010012b
domin <dominikdorawa12@gmail.com>
2024-06-04 23:35:29 +0200

    dodanie index.php i styles.css wraz z navbarem

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
-echo "siemasaa";
-?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Spotify_clone</title>
+</head>
+<body>
+<div class="container">
+
+    <nav class="navbar"></nav>
+    <nav class="navbar-left">
+        <div class="logo">Logo</div>
+        <button class="nav-btn">Home</button>
+        <button class="nav-btn">Szukaj</button>
+        <div class="navbar-right">
+            <button class="nav-btn">Zaloguj się</button>
+            <button class="nav-btn">Zarejestruj się</button>
+        </div>
+    </nav>
+
+
+</div>
+</body>
+
+</html>
+
+
