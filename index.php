a81b04a283bc255c52cc7e18e1ac854493080439
domin <dominikdorawa12@gmail.com>
2024-06-07 14:39:57 +0200

    dodanie paska nawigazyjnego wraz z guzikami do zalogowania sie (bez funkcjonalnosci) oraz wprowadzenie kilki zmian w css i dodanie styli do wyzej wymienionego paska

--- index.php
+++ index.php
@@ -1,102 +1,121 @@
 <!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/841f4f2efe.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="styles.css">
-    <title>Spotify_clone</title>
-</head>
-<body>
+    <html lang="pl" xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/841f4f2efe.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="styles.css">
+        <title>Spotify_clone</title>
+    </head>
+    <body>
 
-<div class="menu">
-    <div class="gora-menu">
-        <div class="logo">
-            <a href="#">
-                <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt="logo">
-            </a>
-        </div>
-        <div class="nawigacja">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class="fa fa-home"></span>
-                        <span class="ikony">Home</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="fa fa-search"></span>
-                        <span class="ikony">Szukaj</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-
-
-    <div class="dol-menu">
-        <div class="nawigacja">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class="fa-solid fa-book"></span>
-                        <span class="ikony">Biblioteka</span>
-                    </a>
-                    <a href="#">
-                        <span class="fa-solid fa-plus"></span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div class="nawigacja">
-            <div class="utworz_playliste">
-                <p class="duzy_tekst">Utwórz swoją pierwszą playlistę</p>
-                <p class="maly_tekst">To proste, pomożemy Ci</p>
+    <div class="menu">
+        <div class="gora-menu">
+            <div class="logo">
+                <a href="index.php" target="_blank">
+                    <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt="logo">
+                </a>
+            </div>
+            <div class="nawigacja">
                 <ul>
                     <li>
-                        <button class="btn">Utwórz playlistę</button>
+
+                        <span class="fa fa-home"></span>
+                        <span class="ikony">Home</span>
+
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-search"></span>
+                            <span class="ikony">Szukaj</span>
+                        </a>
                     </li>
                 </ul>
             </div>
         </div>
-        <div class="regulaminy">
-            <ul>
-                <li>
-                    <a href="https://www.spotify.com/pl/legal/end-user-agreement/">Kwestie prawne</a>
-                </li>
-                <li>
-                    <a href="https://www.spotify.com/pl/safetyandprivacy/reporting-content">Centrum ochrony prywatności i bezpieczeństwa</a>
-                </li>
-                <li>
-                    <a href="https://www.spotify.com/pl/legal/privacy-policy/">Polityka prywatności</a>
-                </li>
-                <li>
-                    <a href="#">Ustawienie plików cookie</a>
-                </li>
-                <li>
-                    <a href="https://www.spotify.com/pl/legal/privacy-policy/#s3">O reklamach</a>
-                </li>
-                <li>
-                    <a href="https://www.spotify.com/pl/accessibility">Dostępność</a>
-                </li>
-                <li>
-                    <a href="#">Pliki cookies</a>
-                </li>
-            </ul>
+
+
+        <div class="dol-menu">
+            <div class="nawigacja">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="fa-solid fa-book"></span>
+                            <span class="ikony">Biblioteka</span>
+                        </a>
+                        <a href="#">
+                            <span class="fa-solid fa-plus"></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="nawigacja">
+                <div class="utworz_playliste">
+                    <p class="duzy_tekst">Utwórz swoją pierwszą playlistę</p>
+                    <p class="maly_tekst">To proste, pomożemy Ci</p>
+                    <ul>
+                        <li>
+                            <button class="btn">Utwórz playlistę</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="regulaminy">
+                <ul>
+                    <li>
+                        <a href="https://www.spotify.com/pl/legal/end-user-agreement/">Kwestie prawne</a>
+                    </li>
+                    <li>
+                        <a href="https://www.spotify.com/pl/safetyandprivacy/reporting-content">Centrum ochrony prywatności i bezpieczeństwa</a>
+                    </li>
+                    <li>
+                        <a href="https://www.spotify.com/pl/legal/privacy-policy/">Polityka prywatności</a>
+                    </li>
+                    <li>
+                        <a href="#">Ustawienie plików cookie</a>
+                    </li>
+                    <li>
+                        <a href="https://www.spotify.com/pl/legal/privacy-policy/#s3">O reklamach</a>
+                    </li>
+                    <li>
+                        <a href="https://www.spotify.com/pl/accessibility">Dostępność</a>
+                    </li>
+                    <li>
+                        <a href="#">Pliki cookies</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="jezyk">
+                <button type="button"  class="jezyk_btn">
+                    <span class="fa-solid fa-globe"></span>
+                    Polski
+                </button>
+            </div>
         </div>
 
-        <div class="jezyk">
-            <button class="jezyk_btn">
-                <span class="fa-solid fa-globe"></span>
-                Polski
-            </button>
-
-        </div>
     </div>
 
 
-</div>
+    <div class="container">
+        <div class="topbar">
+            <div class="strzalki-btn">
+                <button type="button" class="fa-solid fa-chevron-left"></button>
+                <button type="button" class="fa-solid fa-chevron-right"></button>
+            </div>
 
-</body>
+            <div class="navbar">
+                <ul>
+                    <li>
+                        <a href="#">Zarejestruj się</a>
+                    </li>
+                </ul>
+                <button type="button">Zaloguj się</button>
+            </div>
+        </div>
+
+
+
+
+    </body>
 </html>
